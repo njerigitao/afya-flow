814ce0cff8bccc4891b81e34e4c9b157edcfd37c
njerigitao <agnesgitau40@gmail.com>
2025-04-25 14:53:18 +0300

    Recreate database

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'database.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'afya.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
