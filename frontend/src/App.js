12ee0fa024de3e75a339db1725fcf475039ea077
njerigitao <agnesgitau40@gmail.com>
2025-04-26 20:35:07 +0300

    Install Chakra UI and configure ChakraProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link as RouterLink } from "react-router-dom";
+import { Box, Flex, Heading, Link, Spacer } from "@chakra-ui/react";
+import Home from "./pages/Home";
+import Programs from "./pages/Programs";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box minH="100vh" bg="gray.100">
+      <Flex as="nav" bg="blue.600" color="white" p={4} align="center">
+        <Heading size="md">Afya Flow</Heading>
+        <Spacer />
+        <Flex gap={4}>
+          <Link as={RouterLink} to="/" _hover={{ textDecoration: "underline" }}>
+            Clients
+          </Link>
+          <Link as={RouterLink} to="/programs" _hover={{ textDecoration: "underline" }}>
+            Programs
+          </Link>
+        </Flex>
+      </Flex>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </Box>
   );
 }
 
 export default App;
+
